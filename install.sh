bed993fc590258ab34a79e344cd593f0f925b0ec
Kohei Tomita <tommy.fmale@gmail.com>
2014-11-24 18:03:11 +0900

    php forbit monkey patching(; ;)

--- install.sh
+++ install.sh
@@ -2,7 +2,5 @@
 
 WORD_PRESS_ROOT=$(dirname $0)/../
 
-# move original file
-cp ${WORD_PRESS_ROOT}/index.php{,.org}
 # install extend file
-cp ${WORD_PRESS_ROOT}/extend/index.php ${WORD_PRESS_ROOT}/index.php
+cp ${WORD_PRESS_ROOT}/extend/wp-includes/option.php ${WORD_PRESS_ROOT}/wp-includes/option.php
